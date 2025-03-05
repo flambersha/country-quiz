f04e4cb20e30a8042902b23adfaa76608b266a43
flambersha <flambersha@gmail.com>
2025-03-05 09:56:05 +0200

    changed footer text color

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,7 @@
 <script setup></script>
 <template>
     <footer class="mt-auto">
-        <div class="flex flex-col items-center bg-(--footer-bg) text-(--main-text) p-[24px] gap-5">
+        <div class="flex flex-col items-center bg-(--footer-bg) text-gray-300 p-[24px] gap-5">
             <div>2025 Country Quiz</div>
             <div>Inspired by <a href="https://jetpunk.com" target="_blank" class="underline decoration-dotted">JetPunk</a></div>
         </div>
